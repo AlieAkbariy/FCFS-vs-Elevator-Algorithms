e808166040c4ce19896681d67ee283120f0e6706
Ali Akbari <alieakbariy@gmail.com>
2023-04-04 17:52:43 +0330

    implement base of analysis class

---
+++ ProjectPart2/Analysis.py
@@ -0,0 +1,17 @@
+from Algorithm.ElevatorAlgorithm import Elevator
+from Algorithm.FirstComeFirstServiced import FirstComeFirstServiced
+from GenerateDataset import Dataset
+
+
+class Analysis:
+    @staticmethod
+    def start(dataset_path, starting_position):
+        requests_list = Dataset.read_dataset(dataset_path)
+
+        for request_list in requests_list:
+            elevator = Elevator(4.17, 0.13, 4000.0, starting_position, request_list.copy())
+            fcfs = FirstComeFirstServiced(4.17, 0.13, 4000.0, starting_position, request_list.copy())
+
+            elevator.start()
+            fcfs.start()
+            # todo: Save result to attribute of this class
