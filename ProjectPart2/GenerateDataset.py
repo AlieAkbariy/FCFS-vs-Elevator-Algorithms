0faeee1fbce5068bb74c52b96a13208c9275eded
MooNice <shimoshebr@gmail.com>
2023-04-04 12:17:25 +0330

    make read_dataset independent.

--- ProjectPart2/GenerateDataset.py
+++ ProjectPart2/GenerateDataset.py
@@ -10,6 +10,10 @@ class Dataset:
 
     def generate(self):
         dataset = Workbook()
+
+        dataset['Sheet']['A1'] = self.test_case
+        dataset['Sheet']['A2'] = self.request_num_per_test_case
+
         for i in range(self.test_case):
             sheet_name = 'test_case' + str(i + 1)
             sheet = dataset.create_sheet(sheet_name)
@@ -17,29 +21,40 @@ class Dataset:
             for j in range(self.request_num_per_test_case):
                 disk_position = random.randrange(1, 655356)
                 time = random.randrange(1, 100)
+
                 disk_index = 'A' + str(j + 1)
                 time_index = 'B' + str(j + 1)
+
                 sheet[disk_index] = disk_position
                 sheet[time_index] = time
 
         dataset.save(self.dataset_path)
 
-    def read_dataset(self):
-        all_requests = list()
-        dataset = load_workbook(self.dataset_path)
-        for i in range(self.test_case):
-            sheet_name = 'test_case' + str(i + 1)
-            sheet = dataset[sheet_name]
-            requests = list()
 
-            for j in range(self.request_num_per_test_case):
-                request = list()
-                disk_index = 'A' + str(j + 1)
-                time_index = 'B' + str(j + 1)
-                disk_position = sheet[disk_index].value
-                time = sheet[time_index].value
-                request.append(float(disk_position))
-                request.append(float(time))
-                requests.append(request)
-            all_requests.append(requests)
-        return all_requests
+def read_dataset(dataset_path):
+    all_requests = list()
+    dataset = load_workbook(dataset_path)
+
+    test_case = int(dataset['Sheet']['A1'].value)
+    request_num_per_test_case = int(dataset['Sheet']['A2'].value)
+
+    for i in range(test_case):
+        sheet_name = 'test_case' + str(i + 1)
+        sheet = dataset[sheet_name]
+        requests = list()
+
+        for j in range(request_num_per_test_case):
+            request = list()
+
+            disk_index = 'A' + str(j + 1)
+            time_index = 'B' + str(j + 1)
+
+            disk_position = sheet[disk_index].value
+            time = sheet[time_index].value
+
+            request.append(float(disk_position))
+            request.append(float(time))
+            requests.append(request)
+        all_requests.append(requests)
+
+    return all_requests
--- ProjectPart2/main.py
+++ ProjectPart2/main.py
@@ -1,7 +1,8 @@
-from ProjectPart2.GenerateDataset import Dataset
+from ProjectPart2.GenerateDataset import Dataset, read_dataset
 
 if __name__ == '__main__':
-    a = Dataset('../dataset.xlsx', 3, 3)
+    a = Dataset('../dataset.xlsx', 3, 5)
     a.generate()
-    b = a.read_dataset()
-    print(b)
+    b = read_dataset('../dataset.xlsx')
+    for i in b:
+        print(i)
