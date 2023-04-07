97092157f0840e2b1d84fb1309a2ae02484d4bf3
MooNice <shimoshebr@gmail.com>
2023-04-07 12:11:52 +0330

    Generate Full Datasets

---
+++ ProjectPart2/FullDatasetGenerator.py
@@ -0,0 +1,21 @@
+from ProjectPart2.GenerateDataset import Dataset
+
+
+class FullDatasetGenerator:
+    def __init__(self, dir_name, number_of_datasets, request_num):
+        self.dir_name = dir_name
+        self.number_of_datasets = number_of_datasets
+        self.request_num = request_num
+
+    def start(self):
+        path = f'../{self.dir_name}/'
+
+        file = open(path + "README.txt", "w")
+        file.write("oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n" +
+                   f"Number Of Datasets:  {self.number_of_datasets}\n" +
+                   f"Number Of Requests: {self.request_num}\n" +
+                   "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
+
+        for i in range(self.number_of_datasets):
+            dataset = Dataset(path + f'dataset-{i}.xlsx', self.number_of_datasets, self.request_num)
+            dataset.generate()
--- ProjectPart2/main.py
+++ ProjectPart2/main.py
@@ -1,19 +1,41 @@
 from ProjectPart2.GenerateDataset import Dataset
 from ProjectPart2.Output import Output
+from ProjectPart2.FullDatasetGenerator import FullDatasetGenerator
 
 if __name__ == '__main__':
+    pass
     # a = Dataset('../dataset.xlsx', 3, 5)
     # a.generate()
     # b = Dataset.read_dataset('../dataset.xlsx')
     # for i in b:
     #     print(i)
 
-    path = '../dataset.xlsx'
-    dataset = Dataset(path, 100, 5)
-    dataset.generate()
-    rotation_latency, transfer_time, seek_time, starting_position = 4.17, 0.13, 4000.0, 8000
+    # path = '../dataset.xlsx'
+    # dataset = Dataset(path, 100, 5)
+    # dataset.generate()
+    # rotation_latency, transfer_time, seek_time, starting_position = 4.17, 0.13, 4000.0, 8000
+    #
+    # output = Output(path, starting_position, rotation_latency, transfer_time, seek_time)
+    #
+    # output.plot_full_data()
+    # output.plot_compare_data()
 
-    output = Output(path, starting_position, rotation_latency, transfer_time, seek_time)
-
-    output.plot_full_data()
-    output.plot_compare_data()
+    # Make Dataset
+    #
+    # dir_name = 'Dataset-1'
+    # FullDatasetGenerator(dir_name, 100, 10).start()
+    #
+    # dir_name = 'Dataset-2'
+    # FullDatasetGenerator(dir_name, 100, 15).start()
+    #
+    # dir_name = 'Dataset-3'
+    # FullDatasetGenerator(dir_name, 100, 20).start()
+    #
+    # dir_name = 'Dataset-4'
+    # FullDatasetGenerator(dir_name, 100, 25).start()
+    #
+    # dir_name = 'Dataset-5'
+    # FullDatasetGenerator(dir_name, 100, 30).start()
+    #
+    # dir_name = 'Dataset-6'
+    # FullDatasetGenerator(dir_name, 500, 25).start()
