68309d2a424a7cc84805ec3af9c3764bde65fba0
Ali Akbari <alieakbariy@gmail.com>
2023-04-11 01:43:46 +0330

    fix bug in create dataset

--- ProjectPart2/FullDatasetGenerator.py
+++ ProjectPart2/FullDatasetGenerator.py
@@ -1,20 +1,22 @@
 from ProjectPart2.GenerateDataset import Dataset
+import os
 
 
 class FullDatasetGenerator:
-    def __init__(self, dir_name, number_of_datasets, request_num):
+    def __init__(self, dir_name, test_case_number, request_num):
         self.dir_name = dir_name
-        self.number_of_datasets = number_of_datasets
+        self.test_case_number = test_case_number
         self.request_num = request_num
 
     def start(self):
         path = f'../Datasets/{self.dir_name}/'
-
+        if not os.path.exists(path):
+            os.mkdir(path)
         file = open(path + "README.txt", "w")
         file.write("oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo\n" +
-                   f"Number Of Datasets:  {self.number_of_datasets}\n" +
+                   f"Number Of Datasets:  {self.test_case_number}\n" +
                    f"Number Of Requests: {self.request_num}\n" +
                    "oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo")
 
-        dataset = Dataset(path + f'dataset.xlsx', self.number_of_datasets, self.request_num)
+        dataset = Dataset(path + f'dataset.xlsx', self.test_case_number, self.request_num)
         dataset.generate()
